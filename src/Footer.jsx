ab9d9db3d94366de9681c7aae124ae5c46e2db98
SivaTejaArla <sivatejaarla@gmail.com>
2024-12-25 22:00:31 +0530

    footer Completed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,19 +1,63 @@
 import React from "react";
+
+import fb from "./assets/fb.png";
+import insta from "./assets/insta.png";
+import wp from "./assets/whatsapp.png";
+
+
 export default function Footer() {
+
+
+const styles = {
+  container :{
+
+    maxWidth: "100vw",
+    maxHeight: "35vh",
+    background: '#AAFFFB',
+    borderRadius : '20px',
+     border : '2px solid white'
+
+
+  },
+  imageContainer : {
+    display: 'flex', 
+    alignItems : 'center',
+    justifyContent: 'center',
+    gap : '2vw',
+    paddingTop : '2vw'
+
+  },
+  detailscontainer : {
+    display: 'flex', 
+    justifyContent: 'center',
+   gap : '20px'
+  },
+  images : {
+    height : '50px',
+
+  }
+}
+
   return (
     <React.StrictMode>
-      <div
-        className="rounded-box"
-        style={{
-          maxWidth: "100vw",
-          height: "100px",
-          border: "2px solid black",
-          backgroundColor: "lightblue",
-          borderRadius: "10px",
-          padding: "15px",
-        }}
-      >
-        This box has rounded corners!
+      <div style= {styles.container}>
+        <div style={styles.imageContainer}>
+          <img src={fb} alt="Faceboook" style={styles.images}/>
+          <img src={insta} alt="Instagram" style={styles.images} />
+          <img src={wp} alt="Whatsapp" style={styles.images}/>
+        </div>
+        <div style= {styles.detailscontainer}>
+          <h3>Info</h3> 
+          <h3>Marketing</h3>
+          <h3>Support</h3>
+        </div>
+        <div style= {styles.detailscontainer}>
+          <h3>Terms of Use</h3> 
+          <h3>privacy Policy</h3>
+        </div>
+        <div style= {styles.detailscontainer}>
+          <h3>© 2024 Siva Teja Arla</h3> 
+        </div>
       </div>
     </React.StrictMode>
   );
