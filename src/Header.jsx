e703a0354423a58003260dc6a9ad86a6a9aaafa6
SivaTejaArla <sivatejaarla@gmail.com>
2024-12-25 13:42:11 +0530

    Header Completed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,42 @@
 import React from "react";
+import ButtonDesign from "./components/Button";
 export default function Header() {
-
-
+  const styles = {
+    container: {
+      display: 'flex', // Use Flexbox for the container
+      justifyContent: 'space-between', // Distribute space between items
+      maxWidth: '100vw',
       
+      padding : '5vh',
+    },
+    leftContainer: {
+      display: 'flex', // Use Flexbox for the left side container
+      alignItems: 'flex-start', // Align items to the left
+    },
+    rightItem: {
+      marginLeft: 'auto', // Push the right item to the far right
+    backgroundColor: 'lightgreen',
+    padding: '10px',
+    borderRadius: '5px',
+    },
+  }
+
   return (
     <React.StrictMode>
-      <div className="rounded-box" style={{maxWidth: '100vw', height: '100px', border: '2px solid black',  backgroundColor: 'lightblue', borderRadius : '10px' , padding : '15px'}}>
-    This box has rounded corners!
-  </div>
+      <div className="rounded-box" style={styles.container}>
 
+        <div style = {styles.leftContainer}>
+        <ButtonDesign name="Home" />
+        <ButtonDesign name="Contact Us" />
+        <ButtonDesign name="Orders" />
+
+        </div>
+ 
+
+        <ButtonDesign name ="Login" style = {styles.rightItem}></ButtonDesign>
+    
+      </div>
+   
     </React.StrictMode>
   );
 }
